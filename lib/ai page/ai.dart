46d5b00557d9b95062174c3bd10fe963ea72116b
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-10-29 14:16:29 +0500

    feat: integrate temporary gemini AI

--- lib/ai page/ai.dart
+++ lib/ai page/ai.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AI extends StatefulWidget {
   AI({super.key});
@@ -14,7 +18,7 @@ class _AIState extends State<AI> {
   ChatUser currentUser = ChatUser(id: '0', firstName: 'User');
 
   ChatUser geminiUser =
-      ChatUser(id: '1', firstName: 'Gemini', profileImage: 'assets/3.png');
+      ChatUser(id: '1', firstName: 'HR Bot', profileImage: 'assets/3.png');
 
   List<ChatMessage> messages = [];
 
@@ -32,12 +36,63 @@ class _AIState extends State<AI> {
 
   Widget _buildUI() {
     return DashChat(
-        currentUser: currentUser, onSend: _sendMessage, messages: messages);
+        inputOptions: InputOptions(trailing: [
+          IconButton(onPressed: _sendMediaMessage, icon: Icon(Icons.image))
+        ]),
+        currentUser: currentUser,
+        onSend: _sendMessage,
+        messages: messages);
   }
 
   void _sendMessage(ChatMessage chatMessage) {
     setState(() {
       messages = [chatMessage, ...messages];
     });
+    try {
+      String question = chatMessage.text;
+      List<Uint8List>? images;
+      if (chatMessage.medias?.isNotEmpty ?? false) {
+        images = [File(chatMessage.medias!.first.url).readAsBytesSync()];
+      }
+      gemini.streamGenerateContent(question).listen((event) {
+        ChatMessage? lastMessage = messages.firstOrNull;
+        if (lastMessage != null && lastMessage.user == geminiUser) {
+          lastMessage = messages.removeAt(0);
+          String response = event.content?.parts?.fold(
+                  "", (previous, current) => "$previous ${current.text}") ??
+              "";
+          lastMessage.text += response;
+          setState(() {
+            messages = [lastMessage!, ...messages];
+          });
+        } else {
+          String response = event.content?.parts
+                  ?.fold("", (previous, current) => "$previous${current.text}") ??
+              "";
+          ChatMessage message = ChatMessage(
+              user: geminiUser, createdAt: DateTime.now(), text: response);
+          setState(() {
+            messages = [message, ...messages];
+          });
+        }
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _sendMediaMessage() async {
+    ImagePicker picker = ImagePicker();
+    XFile? file = await picker.pickImage(source: ImageSource.gallery);
+    if (file != null) {
+      ChatMessage message = ChatMessage(
+          user: currentUser,
+          createdAt: DateTime.now(),
+          text: 'Descrive the picture: ',
+          medias: [
+            ChatMedia(url: file.path, fileName: '', type: MediaType.image)
+          ]);
+      _sendMessage(message);
+    }
   }
 }
