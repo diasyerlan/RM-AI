1129029134c3a8eaa0400816c59b0ca57d00e239
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-11-08 15:26:37 +0500

    feat: add authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:hr_bot_app/auth/auth.dart';
+import 'package:hr_bot_app/auth/login_or_register.dart';
+import 'package:hr_bot_app/authorization/login_page.dart';
+import 'package:hr_bot_app/authorization/register_page.dart';
 import 'package:hr_bot_app/const.dart';
+import 'package:hr_bot_app/firebase_options.dart';
 import 'package:hr_bot_app/navbar.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Gemini.init(apiKey: GEMINI_API_KEY);
   runApp(const MyApp());
 }
@@ -15,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const NavBar(),
+      home: const AuthPage(),
       theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFF2F2F7)),
     );
   }
